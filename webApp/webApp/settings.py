925aa97943fed17ed959a86e7a1269bceb7a9a3c
Hassan <hassansmallah@gmail.com>
2022-06-14 09:36:53 +0300

    max width + height

--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h$2k6hd6$12(=4jiryz5i1jfb11$&_=br195duil24t%d037vf'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
