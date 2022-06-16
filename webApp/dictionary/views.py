828fbb5934b826bceec44d9ab9702f5700543692
Hassan <hassansmallah@gmail.com>
2022-06-16 22:18:28 +0300

    dictionary app

--- webApp/dictionary/views.py
+++ webApp/dictionary/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    # template = loader.get_template('index.html')
+    template = loader.get_template('dictionary.html')
 
-    return HttpResponse('welcome to dictionary app')
+    # return HttpResponse('welcome to dictionary app')
+    return HttpResponse(template.render())
