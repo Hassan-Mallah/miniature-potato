19a05cecddd8a8057085dd387438e4897827264a
Hassan <hassansmallah@gmail.com>
2022-06-22 21:02:33 +0300

    translator page :)

--- webApp/translator/views.py
+++ webApp/translator/views.py
@@ -1,17 +1,23 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from translate import Translator
 
-
-# Create your views here.
 
 def translate(request: HttpRequest):
-    print(request.GET)
-    print(request.POST)
-    if 'text' in request.GET:
-        print(request.GET['text'])
+    context = {}
 
-    context = {
-        'input': 'test input',
-        'output': 'test output'
-    }
+    if 'input' in request.GET and request.GET['input']:
+        translator = Translator(to_lang="tr")  # set language to Turkish
+
+        # in case of error with translator, return empty data
+        try:
+            translation = translator.translate(request.GET['input'])
+        except:
+            return render(request, 'translator.html', context=context)
+
+        context = {
+            'input': request.GET['input'],
+            'output': translation
+        }
+
     return render(request, 'translator.html', context=context)
