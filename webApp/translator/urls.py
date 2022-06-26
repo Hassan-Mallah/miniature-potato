b472e0a8d680fd9b874f133a4633a824887916da
Hassan <hassansmallah@gmail.com>
2022-06-26 21:10:29 +0300

    django forms

--- webApp/translator/urls.py
+++ webApp/translator/urls.py
@@ -5,5 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('get_name', get_name),
+    path('signup', SignUpView.as_view()),
     path('', translate),
+    path('validate_username/$', validate_username),
+
 ]
--- webApp/translator/views.py
+++ webApp/translator/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 from translate import Translator
 from django.http import HttpResponseRedirect
 from .forms import NameForm
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+
+class SignUpView(CreateView):
+    template_name = 'signup.html'
+    form_class = UserCreationForm
+
+
+def validate_username(request):
+    print('validate_username')
+    username = request.GET.get('username', None)
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(data)
 
 
 def translate(request: HttpRequest):
