2b277034d96b621434a4dcf80d3e9f829c4c3b38
Hassan <hassansmallah@gmail.com>
2022-07-24 21:06:18 +0300

    news.html

--- webApp/newsApp/views.py
+++ webApp/newsApp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from gnewsclient import gnewsclient
 
 
 # Create your views here.
 
 def news(request: HttpRequest):
-    return render(request, 'news.html')
+    # declare a NewsClient object
+    # client = gnewsclient.NewsClient(
+    #     language='english',
+    #     location='turkey',
+    #     topic='Business',
+    #     max_results=5)
+    #
+    # # get news feed
+    # news = client.get_news()
+    #
+    # for item in news:
+    #     print("Title : ", item['title'])
+    #     print("Link : ", item['link'])
+    #     print("")
+
+    # in html file, you use key names directly :)
+    context = {
+        "news": "This is news"
+    }
+    return render(request, 'news.html', context=context)
